0e83e6b629f0032af728e200cd66d188fe6c46a5
hadar-goldstein <hadargol71@gmail.com>
2025-06-04 19:44:31 +0300

    CSS to vacations is added

--- Frontend/src/Components/DataArea/VacationCard/VacationCard.tsx
+++ Frontend/src/Components/DataArea/VacationCard/VacationCard.tsx
@@ -1,19 +1,36 @@
 import { VacationModel } from "../../../Models/VacationModel";
 import { calculate } from "../../../Utils/Calculate";
 import "./VacationCard.css";
+import { CalendarCheck, Info, CurrencyDollar, MapPin } from 'phosphor-react';
+
 
 type VacationCardProps = {
-	vacation: VacationModel;
+    vacation: VacationModel;
 };
 
 export function VacationCard(props: VacationCardProps) {
     return (
         <div className="Card">
-            <img src={props.vacation.imageUrl} crossOrigin="anonymous"></img>
-			<div>{props.vacation.destination}</div>
-			<div>{calculate.formatDateRangeAndNights(props.vacation.startDate, props.vacation.endDate)}</div>
-			<div>{props.vacation.description}</div>
-			<div>{props.vacation.price}</div>
+            <div className="ImageContainer">
+                <img src={props.vacation.imageUrl} crossOrigin="anonymous"></img>
+            </div>
+            <div className="destination-container">
+                <MapPin size={22} />
+                <div>{props.vacation.destination}</div>
+            </div>
+            <div className="destination-container">
+                <CalendarCheck size={22} />
+                <div>{calculate.formatDateRangeAndNights(props.vacation.startDate, props.vacation.endDate)}</div>
+            </div>
+            <div className="test">
+                <Info size={22} />
+                <p className="text">{props.vacation.description}</p>
+            </div>
+
+            <div className="destination-container">
+                <CurrencyDollar size={22} />
+                <div>{props.vacation.price}</div>
+            </div>
         </div>
     );
 }
