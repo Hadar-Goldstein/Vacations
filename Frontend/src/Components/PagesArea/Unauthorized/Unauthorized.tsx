52d28e19d3937ab92397d49b6e43cb023ef2f71f
hadar-goldstein <hadargol71@gmail.com>
2025-06-07 12:48:21 +0300

    typo fixed

--- Frontend/src/Components/PagesArea/Unauthorized/Unauthorized.tsx
+++ Frontend/src/Components/PagesArea/Unauthorized/Unauthorized.tsx
@@ -15,7 +15,7 @@ export function Unauthorized(): JSX.Element {
             <img src={background} className="homeImage"></img>
 
             <div className="container">
-                <p className="unauthorized-title">O P P S . . .</p>
+                <p className="unauthorized-title">O O P S . . .</p>
                 <p className="unauthorized-subtitle">You're Not Logged In</p>
                 <p className="unauthorized-title">Y O U R   N E X T   V A C A T I O N   I S   O N E   C L I C K   A W A Y</p>
                 <button className="unauthorized-button" onClick={startButton} >T A K E   M E   I N</button>
