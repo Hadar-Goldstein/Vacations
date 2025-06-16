e27cf0b15f77308345c64fd05c5258aa902107ba
hadar-goldstein <hadargol71@gmail.com>
2025-06-16 19:42:48 +0300

    menu css fix

--- Frontend/src/Components/LayoutArea/Menu/Menu.tsx
+++ Frontend/src/Components/LayoutArea/Menu/Menu.tsx
@@ -1,32 +1,34 @@
-import { NavLink } from "react-router-dom";
-import "./Menu.css";
-import gif from "../../../Assets/Images/hotel-unscreen.gif"
-import { UserMenu } from "../../UserArea/UserMenu/UserMenu";
-import { AppState } from "../../../Redux/Store";
 import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import gif from "../../../Assets/Images/hotel-unscreen.gif";
 import { UserModel } from "../../../Models/UserModel";
+import { AppState } from "../../../Redux/Store";
+import { UserMenu } from "../../UserArea/UserMenu/UserMenu";
+import "./Menu.css";
 
 export function Menu() {
 
     const user = useSelector<AppState, UserModel>(store => store.user);
-    
+
 
 
     return (
         <div className="Menu">
-            
+
             <div className="MenuLinks">
-            <NavLink to="/home" className={"MenuLink"}>Home</NavLink>
-            <NavLink to="/vacations" className={"MenuLink"}>Vacations</NavLink>   
-            {user?.role === 1 && <NavLink to="/add-vacation" className={"MenuLink"}>Add Vacation</NavLink>}               
-            {user?.role === 1 && <NavLink to="/reports" className={"MenuLink"}>Reports</NavLink>}               
+                <NavLink to="/home" className={"MenuLink"}>Home</NavLink>
+                <NavLink to="/vacations" className={"MenuLink"}>Vacations</NavLink>
+                {user?.role === 1 && <NavLink to="/add-vacation" className={"MenuLink"}>Add Vacation</NavLink>}
+                {user?.role === 1 && <NavLink to="/reports" className={"MenuLink"}>Reports</NavLink>}
             </div>
 
-            <div className="user-menu-container"><UserMenu></UserMenu></div>
+            <div className="right-section">
+                <UserMenu />
+                <img src={gif} />
+            </div>
 
-            <img src={gif}/>
 
-            
+
         </div>
     );
 }
